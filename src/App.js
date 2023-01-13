9970dff6f4c55af976b5db468a5b68aa3195f15e
Cristóbal Paredes <cristobalgutierreznavarro@gmail.com>
2023-01-13 15:55:35 -0300

    2do Commit: Se crea layaout de SPA, carpetas components y pages consus respectivos archivos; se crea page de NotFound y se realiza prueba de ingreso de una ruta que no existe y se obtienen resultados positivos.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+//Importaciones externas
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+//Importacines internas
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
+import NotFound from "./pages/NotFound";
+
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>Happy Cake</h1>
+      <NavBar/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
